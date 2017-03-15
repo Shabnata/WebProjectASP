1c5a869e72567d563e9d2bf30a2490624393a7ba
masmddr <masmddr@gmail.com>
2017-03-15 16:38:00 +0200

    Added Models

--- WebProjectASP/ShoppingSite/Models/OrderItemModel.cs
+++ WebProjectASP/ShoppingSite/Models/OrderItemModel.cs
@@ -12,20 +12,29 @@ namespace ShoppingSite.Models {
 		[Key]
 		[Required]
 		[Display(Name = "Order ID")]
-		[Column("OrderID")]
+		[Column("OrderID",TypeName = "int",Order = 1)]
+		[DataType(DataType.Text)]
 		public int OrderID { get; set; }
 
 		[Key]
 		[Required]
 		[Display(Name = "SKU")]
-		[Column("SKU")]
+		[Column("SKU", TypeName = "int", Order =2)]
+		[DataType(DataType.Text)]
 		public int SKU { get; set; }
 
 		[Required]
 		[Display(Name = "Quantity")]
-		[Column("Quantity")]
+		[Column("Quantity", TypeName = "int")]
+		[DataType(DataType.Text)]
 		[Range(0, 9999, ErrorMessage = "Quantity must be in the range of 0-9999 items.")]
 		public int Quantity { get; set; }
+
+		[Required]
+		[Display(Name = "Price")]
+		[Column("Price", TypeName ="decimal")]
+		[DataType(DataType.Currency)]
+		public decimal Price { get; set; }
 
 		[ForeignKey("OrderID")]
 		public virtual OrderModel Order { get; set; }
