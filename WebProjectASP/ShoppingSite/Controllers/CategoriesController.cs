8f83a757ad1a3209161ce59432988200c6fd6087
masmddr <masmddr@gmail.com>
2017-03-18 22:23:50 +0200

    Added Categories controller and views

---
+++ WebProjectASP/ShoppingSite/Controllers/CategoriesController.cs
@@ -0,0 +1,127 @@
+﻿using ShoppingSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingSite.Controllers {
+	[Authorize(Roles = "Administrator, Manager")]
+	public class CategoriesController : Controller {
+
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		// GET: Categories
+		public async Task<ActionResult> Index() {
+			return View(await db.Categories.ToListAsync());
+		}
+
+		// GET: Categories/Create
+		[HttpGet]
+		public ActionResult Create() {
+			return View();
+		}
+
+		// POST: Categories/Create/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create([Bind(Include = "CategoryName")] CategoryModel categoryModel) {
+			if(ModelState.IsValid) {
+				db.Categories.Add(categoryModel);
+				await db.SaveChangesAsync();
+				return View("Index");
+			}
+
+			return View("Error");
+		}
+
+		// POST: Categories/Edit/5
+		[HttpGet]
+		public async Task<ActionResult> Edit(int? CategoryID) {
+			if(CategoryID == null) {
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CategoryModel categoryModel = await db.Categories.FindAsync(CategoryID);
+			if(categoryModel == null) {
+				return HttpNotFound();
+			}
+			return View(categoryModel);
+		}
+
+		// POST: Categories/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Edit([Bind(Include = "CategoryID, CategoryName, Logo")] CategoryModel categoryModel) {
+			if(ModelState.IsValid) {
+				db.Entry(categoryModel).State = EntityState.Modified;
+				await db.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(categoryModel);
+		}
+
+		// Categories/Details/5
+		public async Task<ActionResult> Details(int? CategoryID) {
+			if(CategoryID == null) {
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CategoryModel categoryModel = await db.Categories.FindAsync(CategoryID);
+			if(categoryModel == null) {
+				return HttpNotFound();
+			}
+			return View(categoryModel);
+		}
+
+		// GET: Categories/Delete/5
+		public async Task<ActionResult> Delete(int? CategoryID) {
+			if(CategoryID == null) {
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CategoryModel categoryModel = await db.Categories.FindAsync(CategoryID);
+			if(categoryModel == null) {
+				return HttpNotFound();
+			}
+			return View(categoryModel);
+		}
+
+		// POST: Categories/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> DeleteConfirmed(int CategoryID) {
+			CategoryModel categoryModel = await db.Categories.FindAsync(CategoryID);
+			db.Categories.Remove(categoryModel);
+			await db.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing) {
+			if(disposing) {
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		[HttpPost, ActionName("Search")]
+		public async Task<ActionResult> Search(string CategoryName) {
+
+			CategoryModel category = await (from c in db.Categories where c.CategoryName.ToLower() == CategoryName.ToLower() select c).SingleAsync();
+
+			if(category != null) {
+				return RedirectToAction("Details", category.CategoryID);
+			}
+			ViewBag.SearchString = CategoryName;
+			ViewBag.NotFoundError = "Category not found";
+			return View("Index");
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> TypeSearch(string SearchString) {
+			ICollection<string> categories = await (from c in db.Categories where c.CategoryName.ToLower().Contains(SearchString.ToLower()) select c.CategoryName).ToListAsync();
+			return Json(categories);
+		}
+
+	}
+}
