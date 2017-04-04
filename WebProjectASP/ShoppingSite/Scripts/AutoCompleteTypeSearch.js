ca97ac702c191bab6b50aef65ab8c5528098237a
Sheydenis <masmddr@gmail.com>
2017-04-05 00:15:12 +0300

    Changed search to use JQuery for AJAX (Untested)

--- WebProjectASP/ShoppingSite/Scripts/AutoCompleteTypeSearch.js
+++ WebProjectASP/ShoppingSite/Scripts/AutoCompleteTypeSearch.js
@@ -1,4 +1,26 @@
 ﻿function typeSearch(elm) {
+	var url = $(this).attr("path");
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: {
+			SearchString: $(this).val()
+		}
+	}).done(function (data, status) {
+		var responseJSON = eval(data);
+		$(elm).autocomplete({ source: responseJSON });
+	}).fail(function (xmlHttpRequest, statusText, errorThrown) {
+		alert(
+		'Your form submission failed.\n\n'
+		+ 'XML Http Request: ' + JSON.stringify(xmlHttpRequest)
+		+ ',\nStatus Text: ' + statusText
+		+ ',\nError Thrown: ' + errorThrown);
+	});
+}
+
+/*
+function typeSearch(elm) {
 	var xhr = null;
 	if (window.ActiveXObject) {
 		xhr = new ActiveXObject("Microsoft.XMLHTTP");
@@ -23,3 +45,4 @@
 		console.log("xhr = null");
 	}
 }
+*/
